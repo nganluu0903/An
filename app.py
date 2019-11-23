0acbe785283fdfa148c9422ec3f70caeb97a1048
nganluu0903 <50857364+nganluu0903@users.noreply.github.com>
2019-11-23 15:16:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    return render_template("index.html")
-   
+@app.route('/shortofperox1.html')
+def index():
+   return render_template("shortofperox1.html")   
 if __name__ == '__main__':
    app.run()
